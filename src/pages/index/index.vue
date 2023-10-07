78b1cb6c61854195d281dc53b76fa85c178aff46
lybeyond1121 <lybeyond1121@gmail.com>
2023-10-07 11:41:34 +0800

    首页分类

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
 import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
-import { getHomeBannerAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import { ref } from 'vue'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 const BannerList = ref<BannerItem[]>([])
+const CategoryList = ref<CategoryItem[]>([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   BannerList.value = res.result
 }
+
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  CategoryList.value = res.result
+}
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
 <template>
   <CustomNavbar />
   <XtxSwiper :list="BannerList" />
+  <CategoryPanel :list="CategoryList" />
   <view class="index">index</view>
 </template>
 
