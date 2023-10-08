e950a863ce07307c3b15b728c306acee96e655db
lybeyond1121 <lybeyond1121@gmail.com>
2023-10-08 22:46:25 +0800

    商品详情 弹出层交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -3,6 +3,8 @@ import { getGoodsByIdAPI } from '@/services/goods'
 import { onLoad } from '@dcloudio/uni-app'
 import type { GoodsResult } from '@/types/goods'
 import { ref } from 'vue'
+import AddressPanel from './components/AddressPanel.vue'
+import ServicePanel from './components/ServicePanel.vue'
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 // 接受页面参数
@@ -37,6 +39,13 @@ const popup = ref<{
   open: (type?: UniHelper.UniPopupType) => void
   close: () => void
 }>()
+// 弹出层条件渲染
+const popupName = ref<'address' | 'service'>()
+const openPopup = (name: typeof popupName.value) => {
+  // 修改弹出层名称
+  popupName.value = name
+  popup.value?.open()
+}
 </script>
 
 <template>
@@ -73,11 +82,11 @@ const popup = ref<{
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('address')" class="item arrow">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view @tap="popup?.open('bottom')" class="item arrow">
+        <view @tap="openPopup('service')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -148,9 +157,9 @@ const popup = ref<{
     </view>
   </view>
   <uni-popup ref="popup" type="bottom" background-color="#fff">
-    <view>内容1</view>
-    <view>内容2</view>
-    <button @tap="popup?.close()">关闭弹出层</button>
+    <AddressPanel v-if="popupName === 'address'" @close="popup?.close()" />
+    <ServicePanel v-if="popupName === 'service'" @close="popup?.close()" />
+    <!-- <button @tap="popup?.close()">关闭弹出层</button> -->
   </uni-popup>
 </template>
 
